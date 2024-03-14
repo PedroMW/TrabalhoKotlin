eceb222640b8776a999000e451febd1807d12ef6
PEDRO_AUGUSTO_MAXIMO_ASSUNCAO <72200820@OMEGAB27.BARROCA>
2024-03-13 21:16:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/exerccio/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.exerccio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btEntrar: Button = findViewById<Button>(R.id.btEntrar)
+
+        btEntrar.setOnClickListener{
+            val etUsuario = findViewById<EditText>(R.id.Usuario)
+            val etSenha = findViewById<EditText>(R.id.editTextTextPassword)
+
+            if(etUsuario.text.toString() != "" && etSenha.text.toString() != "")
+            {
+                val sysUser = "admin"
+                val sysSenha = "123456"
+
+
+                if(etSenha.text.toString() == sysSenha)
+                {
+                    Toast.makeText(this, "Seja bem vindo" + etUsuario.text, Toast.LENGTH_LONG)
+                }
+                else
+                {
+                    Toast.makeText(this, "Usuario ou senha invalidos", Toast.LENGTH_LONG)
+                }
+            }
+            else {
+                Toast.makeText(this, "digite o usuario e senha", Toast.LENGTH_LONG)
+            }
+
+            val btCadastrar: Button = findViewById<Button>(R.id.btCadastrar)
+
+            btCadastrar.setOnClickListener()
+            {
+                Toast.makeText(this, "digite o usuario e senha", Toast.LENGTH_LONG)
+        }
+
+        }
+
+    }
+
+}
